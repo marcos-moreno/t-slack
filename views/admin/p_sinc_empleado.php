6447c18434f4e6154227fed280e1f3557eb7be96
marcos-moreno <marcos.moreno.gm@gmail.com>
2021-04-09 17:02:10 -0500

    ADD field CONPAC SINC CERBERUS

--- views/admin/p_sinc_empleado.php
+++ views/admin/p_sinc_empleado.php
@@ -147,4 +147,4 @@
         </div> 
       </div>
 </div> 
-<script type="text/javascript" src="../../controller/admin/sinc2_cerberus.js"></script>  
+<script type="text/javascript" src="../../controller/admin/sinc3_cerberus.js"></script>  
